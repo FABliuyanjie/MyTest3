684563323f0da179d797e861183e6d061c8cf4de
FABliuyanjie <1513292400@qq.com>
2014-11-05 11:47:17 +0800

    test1

---
+++ MyTest3Tests/MyTest3Tests.m
@@ -0,0 +1,34 @@
+//
+//  MyTest3Tests.m
+//  MyTest3Tests
+//
+//  Created by 刘艳杰 on 14-11-5.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyTest3Tests : XCTestCase
+
+@end
+
+@implementation MyTest3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
